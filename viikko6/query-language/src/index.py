25539058d4f589414104fa3d662fc5ca59539790
Kristian Cerf <kraastian2@gmail.com>
2024-12-09 23:37:38 +0200

    Tehtvä 5 tehty

--- viikko6/query-language/src/index.py
+++ viikko6/query-language/src/index.py
@@ -1,17 +1,16 @@
 from statistics import Statistics
 from player_reader import PlayerReader
-from matchers import And, HasAtLeast, PlaysIn
+from matchers import And, HasAtLeast, PlaysIn, All, Not, HasFewerThan, Or, QueryBuilder
 
 def main():
     url = "https://studies.cs.helsinki.fi/nhlstats/2023-24/players.txt"
     reader = PlayerReader(url)
     stats = Statistics(reader)
 
-    matcher = And(
-        HasAtLeast(5, "goals"),
-        HasAtLeast(20, "assists"),
-        PlaysIn("PHI")
-    )
+
+    query = QueryBuilder()
+
+    matcher = query.plays_in("NYR").has_at_least(10, "goals").has_fewer_than(20, "goals").build()
 
     for player in stats.matches(matcher):
         print(player)
--- viikko6/query-language/src/matchers.py
+++ viikko6/query-language/src/matchers.py
@@ -8,8 +8,33 @@ class And:
                 return False
 
         return True
+    
+class Not:
+    def __init__(self, *matchers):
+        self._matchers = matchers
 
+    def test(self, player):
+        for matcher in self._matchers:
+            if not matcher.test(player):
+                return True
 
+        return False
+    
+class Or:
+    def __init__(self, *matchers):
+        self._matchers = matchers
+
+    def test(self, player):
+        for matcher in self._matchers:
+            if matcher.test(player):
+                return True
+
+        return False
+
+class All:
+    def test(self, player):
+        return player
+    
 class PlaysIn:
     def __init__(self, team):
         self._team = team
@@ -27,3 +52,33 @@ class HasAtLeast:
         player_value = getattr(player, self._attr)
 
         return player_value >= self._value
+    
+class HasFewerThan:
+    def __init__(self, value, attr):
+        self._value = value
+        self._attr = attr
+
+    def test(self, player):
+        player_value = getattr(player, self._attr)
+
+        return player_value <= self._value
+    
+class QueryBuilder:
+    def __init__(self):
+        self._matcher = All()  
+
+    def plays_in(self, team):
+        self._matcher = And(self._matcher, PlaysIn(team))
+        return self
+
+    def has_at_least(self, value, attr):
+        self._matcher = And(self._matcher, HasAtLeast(value, attr))
+        return self
+
+    def has_fewer_than(self, value, attr):
+        self._matcher = And(self._matcher, HasFewerThan(value, attr))
+        return self
+
+    def build(self):
+        return self._matcher
+
